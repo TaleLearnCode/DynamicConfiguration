c799d0d305355f477514a57d2de30473cc825700
Chad Green <chadgreen@chadgreen.com>
2020-05-18 12:16:54 -0400

    TaleLearnCode Stream 5/18/2020 - Hello World sample

---
+++ samples/sdk-samples/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿using Azure.Data.AppConfiguration;
+using System;
+
+namespace HelloWorld
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var client = new ConfigurationClient(Settings.AppConfigConnectionString);
+			var setting = new ConfigurationSetting("HelloWorld", "Welcome to App Configuration!");
+
+			client.SetConfigurationSetting(setting);  // Create a configuration setting
+
+			ConfigurationSetting retrievedSetting = client.GetConfigurationSetting("HelloWorld");
+			Console.WriteLine($"Thte value of the configuration setitng is {retrievedSetting.Value}");
+
+			client.DeleteConfigurationSetting("HelloWorld");  // Delete a configuration setting
+
+		}
+	}
+}
