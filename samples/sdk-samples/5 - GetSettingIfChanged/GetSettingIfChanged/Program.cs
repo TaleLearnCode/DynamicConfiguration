25f33d30ee9c984fe93b19360528e43201ae6c55
Chad Green <chadgreen@chadgreen.com>
2020-05-18 13:01:19 -0400

    TaleLearnCode stream 5/18/2020 - GetSettingIfChanged sample

---
+++ samples/sdk-samples/5 - GetSettingIfChanged/GetSettingIfChanged/Program.cs
@@ -0,0 +1,56 @@
+﻿using Azure;
+using Azure.Data.AppConfiguration;
+using HelloWorld;
+using System;
+
+namespace GetSettingIfChanged
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Create a ConfigurationClient
+			ConfigurationClient client = new ConfigurationClient(Settings.AppConfigConnectionString);
+
+			// Get initial setting with ETag
+			ConfigurationSetting setting = client.SetConfigurationSetting("some_key", "initial_value");
+			Console.WriteLine($"setting.ETag is '{setting.ETag}'");
+			Console.ReadLine();
+
+			// Get latest setting
+			ConfigurationSetting latestSetting = GetConfigurationSettingIfChanged(client, setting);
+			Console.WriteLine($"Latest version of setting is {latestSetting}.");
+			Console.ReadLine();
+
+			// Change the setting
+			client.SetConfigurationSetting("some_key", "changed_value");
+			ConfigurationSetting newLatestSetting = GetConfigurationSettingIfChanged(client, setting);
+			Console.WriteLine($"Latest version of setting is {newLatestSetting}.");
+			Console.ReadLine();
+
+			// Get latest setting
+			ConfigurationSetting newestLatestSetting = GetConfigurationSettingIfChanged(client, newLatestSetting);
+			Console.WriteLine($"Latest version of setting is {newestLatestSetting}.");
+			Console.ReadLine();
+
+			// Delete a configuration setting
+			client.DeleteConfigurationSetting("some_key");
+
+		}
+
+		public static ConfigurationSetting GetConfigurationSettingIfChanged(ConfigurationClient client, ConfigurationSetting setting)
+		{
+			Response<ConfigurationSetting> response = client.GetConfigurationSetting(setting, onlyIfChanged: true);
+			int httpStatusCode = response.GetRawResponse().Status;
+			Console.WriteLine($"Received a response code of {httpStatusCode}");
+
+			return httpStatusCode switch
+			{
+				200 => response.Value,
+				304 => setting,
+				_ => throw new InvalidOperationException()
+			};
+		}
+
+	}
+}
---
+++ samples/sdk-samples/5 - GetSettingIfChanged/GetSettingIfChanged/Settings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+	public class Settings
+	{
+
+		public static string AppConfigConnectionString { get; } = "Endpoint=https://appconf-poc.azconfig.io;Id=jBFN-l5-s0:oJZGlEcK2kB560sDdzGs;Secret=qpwSIDhjxTo8xSeEwY+yUVYNmafpcMU8oZxI1/XRa+Q=";
+
+	}
+}
